dc2ee11bcb9766d0be21743eaff190ed2ba06281
rasheeq123 <zehrarasheeq@bbdnitm.ac.in>
2025-06-15 18:50:52 +0530

    Add project files.

---
+++ VehicleServiceManagemnt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleServiceManagemnt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
