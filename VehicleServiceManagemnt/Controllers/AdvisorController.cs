5072314c13caa0ffe1aa956808e765b844554a85
rasheeq123 <zehrarasheeq@bbdnitm.ac.in>
2025-06-24 21:16:42 +0530

    Adding Account and Advisor Controller

---
+++ VehicleServiceManagemnt/Controllers/AdvisorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleServiceManagemnt.Controllers
+{
+    public class AdvisorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
